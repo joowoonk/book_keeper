96d1c499c45d07e9bfd259919495e9941d01c3f4
Your Name <joowoonk@gmail.com>
2021-07-06 22:07:03 -0700

    getting env setup with template

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/joowoonk/book_keeper
+
+go 1.16
+
+require github.com/jinzhu/gorm v1.9.16 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/jinzhu/gorm"
+)
+
+
+type Person struct {
+	gorm.Model
+
+	Name string
+	Email string `gorm:"typevarchar(100);unique_index"`
+	Books []Book
+}
+
+type Book struct {
+	gorm.Model
+
+	Title string
+	Author string
+	CallNumber int `gorm:"unique_index"`
+	PersonID int 
+}
+
+func main(){
+	// Loading environment variables 
+	dialet := os.Getenv("DIALECT")
+	host := os.Getenv("HOST")
+	dbPort := os.Getenv("DBPORT")
+	user := os.Getenv("USER")
+	dbName := os.Getenv("NAME")
+	password := os.Getenv("PASSWORD")
+	fmt.Println(dialet,host,dbPort,user,dbName,password,"yes")
 }
