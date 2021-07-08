a5b752058c9e14f4b33df8c14c24545730470ef5
Your Name <joowoonk@gmail.com>
2021-07-07 23:21:00 -0700

    found way to import from env file to main but testing if env can be hidden

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/joowoonk/book_keeper
 
 go 1.16
 
-require github.com/jinzhu/gorm v1.9.16 // indirect
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 )
 
 
@@ -29,29 +30,40 @@ type Book struct {
 var db *gorm.DB
 var err error
 
+func goDotEnvVariable(key string) string {
+
+  // load .env file
+  err := godotenv.Load(".env")
+
+  if err != nil {
+    log.Fatalf("Error loading .env file")
+  }
+
+  return os.Getenv(key)
+}
+
+
 func main(){
 	// Loading environment variables 
-	dialect := os.Getenv("DIALECT")
-	host := os.Getenv("HOST")
-	dbPort := os.Getenv("DBPORT")
-	user := os.Getenv("USER")
-	dbName := os.Getenv("NAME")
-	password := os.Getenv("PASSWORD")
+	godotenv.Load(".env")
+	dialect := goDotEnvVariable("DIALECT")
+	host := goDotEnvVariable("HOST")
+	dbPort := goDotEnvVariable("DBPORT")
+	user := goDotEnvVariable("USER")
+	dbName := goDotEnvVariable("NAME")
+	// password := goDotEnvVariable("NUFF")
+	fmt.Println(dialect,host,dbPort,user,dbName)
+	// dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", host, user, dbName,password, dbPort)
 
-	// Database connection string
-	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", host, user, dbName,password, dbPort)
+	// // Openning connection to database
+	// db, err = gorm.Open(dialect, dbURI)
 
-	// Openning connection to database
-	db, err = gorm.Open(dialect, dbURI)
+	// if err != nil{
+	// 	log.Fatal(err)
+	// }else{
+	// 	fmt.Println("Successfully connected to database")
+	// }
 
-	if err != nil{
-		log.Fatal(err)
-	}else{
-		fmt.Println("Successfully connected to database")
-	}
-
-	// Close connection to database when the main function finisheds
-	defer db.Close()
-
-
+	// // Close connection to database when the main function finisheds
+	// defer db.Close()
 }
